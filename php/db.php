136f9b32de21ddacfd5e69d5cb6d1f51d45d005f
doktorvasi <abdulwasiaaqrah@gmail.com>
2021-01-23 21:58:05 +0300

    --- Auto-Git Commit ---

--- php/db.php
+++ php/db.php
@@ -8,7 +8,7 @@ class db {
     protected $query_closed = TRUE;
 	public $query_count = 0;
 
-	public function __construct($dbhost = 'localhost', $dbuser = 'root', $dbpass = '', $dbname = '', $charset = 'utf8') {
+	public function __construct($dbhost = '40.78.80.174', $dbuser = 'sinemagiris', $dbpass = 'Er0TeM6Xdofuwnbj', $dbname = 'sinema2', $charset = 'utf8') {
 		$this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 		if ($this->connection->connect_error) {
 			$this->error('Failed to connect to MySQL - ' . $this->connection->connect_error);
